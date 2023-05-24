1e592cf576db241235ac42056e6ae923df5712ea
BrownieBearApocolypse <maddyellicott195@gmail.com>
2023-05-24 12:09:41 +1000

    brute forcing zoom

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -10,30 +10,18 @@ public class ButtonManager : MonoBehaviour
     private DisplayImage currentDisplay;
     private ObjectManager objectManager;
     public GameObject[] changeview;
-
-    private float initialCameraSize;
-    private Vector3 initialCameraPosition;
+    
     
     void Start()
     {
         currentDisplay = GameObject.Find("displayImage").GetComponent<DisplayImage>();
-        objectManager = GameObject.Find("script holder").GetComponent<ObjectManager>();
-        initialCameraSize = Camera.main.orthographicSize;
-        initialCameraPosition = Camera.main.transform.position;
+        objectManager = GameObject.Find("script holder").GetComponent<ObjectManager>();        
     }
    
     public void OnClickReturn()
     {       
         if (currentDisplay.CurrentState == DisplayImage.State.zoom)
         {
-            GameObject.Find("displayImage").GetComponent<DisplayImage>().CurrentState = DisplayImage.State.normal;
-            var zoomInObjects = FindObjectsOfType<ZoomInObject>();
-            foreach(var zoomInObject in zoomInObjects)
-            {
-                zoomInObject.gameObject.layer = 0;
-            }
-            Camera.main.orthographicSize = initialCameraSize;
-            Camera.main.transform.position = initialCameraPosition;
             foreach (var change in changeview)
             {
                 GetComponent<ChangeView>().enabled = true;
--- Assets/Scripts/ZoomInObject.cs
+++ Assets/Scripts/ZoomInObject.cs
@@ -4,49 +4,32 @@ using UnityEngine;
 
 public class ZoomInObject : MonoBehaviour, IInteractable
 {
-    //goes on zoom object
-    public float ZoomRatio = 0.5f;
     public GameObject[] changeview;
-    //set object this is attached to to Ignore Raycast
+    public GameObject[] cameraNewPos;
+
     public void Interact(DisplayImage currentDisplay)
     {
-        Debug.Log("ClickZoom");
-        Camera.main.orthographicSize *= ZoomRatio;
-        Camera.main.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-        gameObject.layer = 2;
         currentDisplay.CurrentState = DisplayImage.State.zoom;
-        ConstraintCamera();
         foreach (var change in changeview)
         {
             GetComponent<ChangeView>().enabled = false;
         }
-    }
-
-    void ConstraintCamera()
-    {
-        var height = Camera.main.orthographicSize;
-        var width = height * Camera.main.aspect;
-
-        var cameraBounds = GameObject.Find("cameraBounds");
-
-        if (Camera.main.transform.position.x + width > cameraBounds.transform.position.x + cameraBounds.GetComponent<BoxCollider2D>().size.x / 2)
+        for (int i = 0; i < cameraNewPos.Length; i++)
         {
-            Camera.main.transform.position += new Vector3(cameraBounds.transform.position.x + cameraBounds.GetComponent<BoxCollider2D>().size.x / 2 - (Camera.main.transform.position.x + width), 0, 0);
-        }
+            if (cameraNewPos[i].name == currentDisplay.GetComponent<SpriteRenderer>().sprite.name)
+            {
+                
+            }
+            else if (cameraNewPos[i].name == currentDisplay.GetComponent<SpriteRenderer>().sprite.name)
+            {
+               
+            }
+            else
+            {
 
-        if (Camera.main.transform.position.x - width > cameraBounds.transform.position.x - cameraBounds.GetComponent<BoxCollider2D>().size.x / 2)
-        {
-            Camera.main.transform.position += new Vector3(cameraBounds.transform.position.x - cameraBounds.GetComponent<BoxCollider2D>().size.x / 2 + (Camera.main.transform.position.x - width), 0, 0);
-        }
-
-        if (Camera.main.transform.position.x + height> cameraBounds.transform.position.x + cameraBounds.GetComponent<BoxCollider2D>().size.x / 2)
-        {
-            Camera.main.transform.position += new Vector3(cameraBounds.transform.position.x + cameraBounds.GetComponent<BoxCollider2D>().size.x / 2 - (Camera.main.transform.position.x + height), 0, 0);
-        }
-
-        if (Camera.main.transform.position.x - height > cameraBounds.transform.position.x - cameraBounds.GetComponent<BoxCollider2D>().size.x / 2)
-        {
-            Camera.main.transform.position += new Vector3(cameraBounds.transform.position.x - cameraBounds.GetComponent<BoxCollider2D>().size.x / 2 + (Camera.main.transform.position.x + height), 0, 0);
+            }
         }
     }
+
+    
 }
